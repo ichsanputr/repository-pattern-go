096dd2f647bfbcd24c288e4e152f57fe924fc980
ichsanputr <githubichsan@gmail.com>
2021-03-01 09:22:08 +0700

    add testing filr

--- implementing_test.go
+++ implementing_test.go
@@ -1,6 +1,23 @@
+package implement_repository_pattern
 
+import (
+	"fmt"
+	"testing"
 
+	r "github.com/ichsanputr/repository-pattern-go/repository"
 
+	_ "github.com/go-sql-driver/mysql"
+)
 
+func TestRepositoryPattern(t *testing.T) {
+	db := Connection()
 
+	repostudent := r.NewRepositoryStudentImplement(db)
+	a, err := repostudent.FindById(1)	
 
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(a)
+}
